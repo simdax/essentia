90e00ed557f526140e18e223a86cd5d44548a2fb
nicolas <na@soundx.fr>
2022-11-11 14:36:45 +0100

    build scriptm agai

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+cd `dirname $0`
+
 brew unlink ffmpeg 
 
 brew install pkg-config readline sqlite gdbm freetype libpng eigen libyaml fftw libsamplerate libtag tensorflow
@@ -8,7 +10,9 @@ export PKG_CONFIG_PATH="/opt/homebrew/opt/ffmpeg@2.8/lib/pkgconfig"
 python3.9 -m pip install numpy matplotlib pyyaml
 
 CC=clang CXX=clang++ python3 waf configure --build-static --with-tensorflow --with-python \
---include-algos="FFT,TriangularBands,Spectrum,Windowing,MelBands,Magnitude,NoiseAdder,UnaryOperator,TensorflowInputMusiCNN,TensorToPool,TensorToVectorReal,TensorflowPredictEffnetDiscogs,TensorflowPredict,PoolToTensor,FileOutputProxy,FrameCutter,VectorRealToTensor,Resample"
+--include-algos="Resample,FFT,TriangularBands,Spectrum,Windowing,MelBands,Magnitude,NoiseAdder,UnaryOperator,TensorflowInputMusiCNN,TensorToPool,TensorToVectorReal,TensorflowPredictEffnetDiscogs,TensorflowPredict,PoolToTensor,FileOutputProxy,FrameCutter,VectorRealToTensor"
 
 python3 waf
 python3 waf install
+
+cd -1
