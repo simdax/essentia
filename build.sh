5e575a66a63f1b97160222d2be799a6e8bd97fc4
nicolas <na@soundx.fr>
2022-11-15 19:36:45 +0100

    better build

--- build.sh
+++ build.sh
@@ -1,18 +1,17 @@
 cd `dirname $0`
 
+python="python3.9"
+algo=\
+"Resample,FFT,TriangularBands,Spectrum,Windowing,MelBands,Magnitude,NoiseAdder,\
+UnaryOperator,TensorflowInputMusiCNN,TensorToPool,TensorToVectorReal,\
+TensorflowPredictEffnetDiscogs,TensorflowPredict,PoolToTensor,\
+FileOutputProxy,FrameCutter,VectorRealToTensor"
+
 brew unlink ffmpeg 
-
 brew install pkg-config readline sqlite gdbm freetype libpng eigen libyaml fftw libsamplerate libtag tensorflow
-
 brew install ffmpeg@2.8 
 export PKG_CONFIG_PATH="/opt/homebrew/opt/ffmpeg@2.8/lib/pkgconfig"
-
-python3.9 -m pip install numpy matplotlib pyyaml
-
-CC=clang CXX=clang++ python3 waf configure --build-static --with-tensorflow --with-python \
---include-algos="Resample,FFT,TriangularBands,Spectrum,Windowing,MelBands,Magnitude,NoiseAdder,UnaryOperator,TensorflowInputMusiCNN,TensorToPool,TensorToVectorReal,TensorflowPredictEffnetDiscogs,TensorflowPredict,PoolToTensor,FileOutputProxy,FrameCutter,VectorRealToTensor"
-
-python3 waf
-python3 waf install
-
-cd -1
+$python -m pip install numpy matplotlib pyyaml
+CC=clang CXX=clang++ $python waf configure --build-static --with-tensorflow --with-python --include-algos=$algos
+$python waf
+sudo $python waf install
