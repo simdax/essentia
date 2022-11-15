dc549a57376ff76a119ca631e74a66e67fab0bac
nicolas <na@soundx.fr>
2022-11-15 23:34:58 +0100

    build again

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 cd `dirname $0`
 
 python="python3.9"
-algo=\
-"Resample,FFT,TriangularBands,Spectrum,Windowing,MelBands,Magnitude,NoiseAdder,\
+algos=\
+"FFT,TriangularBands,Spectrum,Windowing,MelBands,Magnitude,NoiseAdder,\
 UnaryOperator,TensorflowInputMusiCNN,TensorToPool,TensorToVectorReal,\
+Resample,\
 TensorflowPredictEffnetDiscogs,\
 TensorflowInputVggish,TensorflowPredictVggish,\
 TensorflowPredict,PoolToTensor,\
@@ -14,6 +15,6 @@ brew install pkg-config readline sqlite gdbm freetype libpng eigen libyaml fftw 
 brew install ffmpeg@2.8 
 export PKG_CONFIG_PATH="/opt/homebrew/opt/ffmpeg@2.8/lib/pkgconfig"
 $python -m pip install numpy matplotlib pyyaml
-CC=clang CXX=clang++ $python waf configure --build-static --with-tensorflow --with-python --include-algos=$algos
+CC=clang CXX=clang++ $python waf configure --build-static --with-tensorflow --with-python --include-algos="$algos"
 $python waf
 sudo $python waf install
